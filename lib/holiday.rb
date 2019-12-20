ef9e4e914a0a7191e8df46b18247f47c2bda7411
Amanda Rolon <abrolon87@gmail.com>
2019-12-20 17:02:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
       holiday_hash[:winter][:christmas] << supply
     end
     if holiday == "New Year's"
-      holiday => supply
+      holiday_hash[:winter][:new_years] << supply
     end
 
 
